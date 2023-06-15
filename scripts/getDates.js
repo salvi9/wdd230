20131516ac1df07a6d96fa9864abd1344a822d04
Danny Hernandez <dxherny@gmail.com>
2023-06-14 18:04:17 -0600

    team activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,26 @@ const options = {
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 document.getElementById('lastModified').textContent = new Date(document.lastModified);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
