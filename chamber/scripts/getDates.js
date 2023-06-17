b560f80cb172b22df2ff2aa82ac01ca0dd035364
Danny Hernandez <dxherny@gmail.com>
2023-06-17 15:55:43 -0600

    chamber hp

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,2 +1,10 @@
 
 document.getElementById('lastModified').textContent = new Date(document.lastModified);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
