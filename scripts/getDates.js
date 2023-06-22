6016617153469aa109578d6dab26b46f97e83660
Danny Hernandez <dxherny@gmail.com>
2023-06-21 18:34:24 -0600

    learning activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -27,3 +27,13 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "☑️";
 	}
 });
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 1;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+}
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
