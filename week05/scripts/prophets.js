f7e8f47330c15f645014ae6aae8ca74cda8f5d1b
Danny Hernandez <dxherny@gmail.com>
2023-07-05 11:10:28 -0600

    prophets

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -4,29 +4,28 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets);
     displayProphets(data.prophets);
 }
 
+getProphetData();
+
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-        let card = document.createElemenmt('selection');
+        let card = document.createElement('selection');
         let fullName = document.createElement('h2')
-        let protrait = document.createElement('img')
+        let portrait = document.createElement('img')
 
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
 
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        portrait.setAttribute('width', '240');
+        portrait.setAttribute('height', '340');
 
         card.appendChild(fullName);
         card.appendChild(portrait);
 
         cards.appendChild(card);
     });
-}
-
-displayProphets;
+};
