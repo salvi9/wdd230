07b6a4439ecf8c86e236fd280352cf6943c1a3a6
Danny Hernandez <dxherny@gmail.com>
2023-07-05 17:42:36 -0600

    s

--- scripts/links.js
+++ scripts/links.js
@@ -4,5 +4,5 @@ const linksURL = "https://salvi9.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = response.json();
-    console.data(data);
+    console.log(data);
 }
