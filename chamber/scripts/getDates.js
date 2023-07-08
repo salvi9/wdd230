91391c1eb29682fbaa62bd5b23b98c38bf875ff0
Danny Hernandez <dxherny@gmail.com>
2023-07-08 15:02:45 -0600

    directory page

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -17,11 +17,68 @@ const cards = document.querySelector('#directory_cards');
 async function getMemberData() {
 	const response = await fetch(url);
 	const data = await response.json();
-	console.log(data.members);
 	displayMembers(data.members);
 }
-
 getMemberData()
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('div');
+        card.classList.add("directory_div")
+        let tags = document.createElement('h3');
+		let tags1 = document.createElement('h3');
+		let tags2 = document.createElement('h3');
+		let tags3 = document.createElement('h3');
+
+        let website = document.createElement('a')
+
+        website.setAttribute('href', `${member.url}`);
+        website.textContent = `Website`
+
+        let portrait = document.createElement('img');
+
+        portrait.setAttribute('src', member.image);
+        portrait.setAttribute('alt', `Portrait of ${member.name}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '240');
+        portrait.setAttribute('height', '340');
+
+        tags.textContent = `Name: ${member.name}`;
+        tags1.textContent = `Address: ${member.address}`;
+        tags2.textContent = `Phone Number: ${member.phone}`;
+        tags3.textContent = `Membership: ${member.membershipLevel}`;
+
+        card.appendChild(website);
+        card.appendChild(portrait);
+        card.appendChild(tags);
+		card.appendChild(tags1);
+		card.appendChild(tags2);
+		card.appendChild(tags3);
+        card.appendChild(website);
+
+
+        cards.appendChild(card);
+    });
+};
+
+/* grid button and list button */
+const gridbutton = document.querySelector("#directory_grid");
+const listbutton = document.querySelector("#directory_list");
+const display = document.querySelector("#directory_cards");
+
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("directory_grid");
+	display.classList.remove("directory_list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+	display.classList.add("directory_list");
+	display.classList.remove("directory_grid");
+}
+
 
 // Tracking visit and Time in between page visit for Discover Page
 let lastVisitedTime = localStorage.getItem('lastVisitedTime');
@@ -84,5 +141,4 @@ function displayRatingValue() {
 }
 
 document.querySelector('#timestamp').textContent = currentTime;
-
 
