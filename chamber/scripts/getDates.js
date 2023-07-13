f9e278b19bb9c27bd00361c86a326261d1d78d4e
Danny Hernandez <dxherny@gmail.com>
2023-07-13 09:55:25 -0600

    website

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -23,8 +23,9 @@ banner.addEventListener("click", () => {
 
 // Display Banner on Mondays, Tuesdays, and Wednesdays
 let banner_day = new Date().getDay();
-if (banner_day >= 1 && banner_day <= 3) {
-    document.querySelector('.current_events').display;
+if (banner_day >= 1 && banner_day <= 2) {
+    banner_display.classList.remove('banner');
+    banner_display.classList.add('banner_none');
 }
 
 // Daily Forecast
